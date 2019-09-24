ebe2e27338bfd41672b655ccad06ecb7c6107c80
Satskiy <satskiy.a@gmail.com>
2019-09-24 20:24:31 +0300

    new test

--- requirements/install.sh
+++ requirements/install.sh
@@ -22,8 +22,7 @@ done
 echo "Install Django requirements..."
 for line in $(cat requirements/django.txt | grep -v \#)
 do
-    #pip install $line #--use-mirrors --allow-all-external --allow-unverified django-admin-tools
-    pip install $line -q Django==$DJANGO
+    pip install $line #--use-mirrors --allow-all-external --allow-unverified django-admin-tools
 done
 
 echo "Install Dev requirements..."
